603e30654fad8e861e3404303cd03b62edcc6027
Medrado <120060198+Medrado011@users.noreply.github.com>
2025-08-21 10:45:35 -0300

    Add files via upload

---
+++ jogadores.java
@@ -0,0 +1,17 @@
+public class jogadores {
+    public static void main(String[] args) {
+        System.out.println("Top 10 melhores jogadores: ");
+        System.out.println();
+        System.out.println();
+        System.out.println("neymar");
+        System.out.println("messi");
+        System.out.println("cristiano ronaldo");
+        System.out.println("ronaldinho");
+        System.out.println("pele");
+        System.out.println("Garrincha");
+        System.out.println("Zinedine Zidane");
+        System.out.println("Diego Maradona");
+        System.out.println("Romario");
+        System.out.println("Ronaldo Fenomeno");
+    }
+}
